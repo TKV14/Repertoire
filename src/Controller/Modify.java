96bb672d117ad8a3a5aec06dbdf61165b55f9b29
TKV <tkv@hotmail.fr>
2013-11-03 17:03:59 +0100

    Ajout d'un test de contact modify

--- src/Controller/Modify.java
+++ src/Controller/Modify.java
@@ -1,5 +1,8 @@
 package Controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,11 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import Metier.Contact;
+
 public class Modify {
 
 	@RequestMapping(method=RequestMethod.GET)
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
-		return new ModelAndView("modify", null);
+		Contact c = new Contact("nom", "prenom", "mail");
+		
+		Map<String, Contact> model = new HashMap<String, Contact>();
+		model.put("contact", c);
+		
+		return new ModelAndView("modify", model);
 	}
 }
